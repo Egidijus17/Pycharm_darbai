8cd5bd7ed43f7b2d341365c7be47a2f39a5bc45c
Egidijus17 <egidijusvilkas17@gmail.com>
2023-06-21 07:14:39 +0300

    initial commit

---
+++ PythonProject17/06.20.DarbasSuFailais.py
@@ -0,0 +1,45 @@
+
+# ---------------------------------- DARBAS SU FAILAIS-----------------------------------
+#
+# Darbas su failais naudijant open funkcija:
+#
+#                                                                    #"a" - append
+# file = open("failopavadinimas.txt", "a", encoding="utf8")                          #"r" - read - nuskaito, "w" - write - paraso
+# content = file.write("Tekstas naujame faile, kuriame išbandome open funkcija")
+# print(content)
+#
+# file = open("failopavadinimas.txt", "a", encoding="utf8")                          #"r" - read - nuskaito, "w" - write - paraso
+# file.write("Tekstas naujame faile, kuriame išbandome open funkcija")      #encoding - supranta ltu raides
+# file.close(                                                              #uzdaro faila, ty baigia veiksma
+#
+#
+# with open("failopavadinimas.txt", "w", encoding="utf8") as file:     #tinkamai atidaro ir uzdaro failus, nedubliuoja teksto
+#     file.write("Tekstas antrajame faile\n")
+#     file.write("Antra eilute\n")
+#     file.write("Trecia eilute\n")
+#
+# filename = input("Iveskite failo pavadinima-> ")
+#
+# try:
+#     with open(filename, "r", encoding="utf8") as file:
+#         content = file.read()
+#         print("File content: ")
+#         print(content)
+# except FileNotFoundError:
+#     print("File not found!")
+# except:
+#     print("Something went wrong!")
+#
+# file = open("Naujasfailas.txt", "w", encoding="utf8")
+# content = file.write("Naujo failo naujas tekstas")
+# print(content)
+# file = ("Iveskite naujo failo pavadinima-> ")
+# try:
+#     with open("Naujasfailas.txt", "w", encoding="utf8") as file:
+#         file.write("Bandymas kurti faila\n")
+#         file.write("Manau pavyko\n")
+#         file.write("Puiku, Egidijau!")
+#         print("Duomenys irasyti")
+# except:
+#     print("Tu dar nesimokai!")
+
